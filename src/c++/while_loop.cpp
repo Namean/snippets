50b5b8679964d79641ccdc6b1c4ebeb601a9599e
Namean <dwilsoncnc@gmail.com>
2024-02-11 18:25:32 -0500

    working on while loops in c++

---
+++ src/c++/while_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int i = 1;
+
+  while (i <= 6)
+  {
+    cout << "Value of variable is is: " << i << endl;
+    i++;
+  }
+
+  return 0;
+}
